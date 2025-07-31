039013389187fccb10b72e810be86a9636351b76
Marcos Avila <m.avila4@unapec.edu.do>
2025-07-31 15:42:34 -0300

    SQLAlchemy Models Done, and Flask-Smorest Serialization Done

--- resources/items.py
+++ resources/items.py
@@ -1,59 +1,79 @@
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError
+
+from db import db
+
 import uuid
 from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 
-from db import items
-
 from schema import ItemSchema, ItemUpdateSchema
+
+from models import ItemModel
 
 bp = Blueprint("Items", __name__, description="Operations on items")
 
 
 @bp.route("/item/<string:item_id>")
 class Item(MethodView):
+    @bp.response(200, ItemSchema)
     def get(self, item_id):
-        try:
-            return items[item_id]
-        except KeyError:
-            abort(404, message="Item not found.")
-
+        item = ItemModel.query.get_or_404(item_id)
+        return item
+    
     def delete(self, item_id):
-        try:
-            del items[item_id]
-            return {"message": "Item deleted."}
-        except KeyError:
-            abort(404, message="Item not found.")
+        item = ItemModel.query.get_or_404(item_id)
+
+        db.session.delete(item)
+        db.session.commit()
+
+        return {"message": "Item deleted"}
 
     @bp.arguments(ItemUpdateSchema)
+    @bp.response(200, ItemSchema)
     def put(self, item_data, item_id):
-        try:
-            item = items[item_id]
+        item = ItemModel.query.get(item_id)
 
-            # https://blog.teclado.com/python-dictionary-merge-update-operators/
-            item |= item_data
+        if item:
+            item.price = item_data["price"]
+            item.name = item_data["name"]
+        else:
+            item = ItemModel(id=item_id, **item_data)
+        
+        db.session.add(item)
+        db.session.commit()
 
-            return item
-        except KeyError:
-            abort(404, message="Item not found.")
+        return item
+    
+#Ejemplo:
+
+    #item = item | item_data
+
+    #Es decir: actualiza item con las claves y valores de item_data.
+    #item = {"name": "Laptop", "price": 1000}
+    #item_data = {"price": 899.99, "stock": 5}
+    #item |= item_data
+    # Resultado:
+    # item == {'name': 'Laptop', 'price': 899.99, 'stock': 5}
+
 
 
 @bp.route("/item")
 class ItemList(MethodView):
+    @bp.response(200, ItemSchema(many=True))
     def get(self):
-        return {"items": list(items.values())}
+        return ItemModel.query.all()
 
     @bp.arguments(ItemSchema)
+    @bp.response(201, ItemSchema)
     def post(self, item_data):
-        for item in items.values():
-            if (
-                item_data["name"] == item["name"]
-                and item_data["store_id"] == item["store_id"]
-            ):
-                abort(400, message=f"Item already exists.")
+        item = ItemModel(**item_data)
+        
+        try:
+            db.session.add(item)
+            db.session.commit()
 
-        item_id = uuid.uuid4().hex
-        item = {**item_data, "id": item_id}
-        items[item_id] = item
+        except SQLAlchemyError:
+            abort(500, message = "An error occured while inserting the item.")
 
         return item
--- schema.py
+++ schema.py
@@ -1,16 +1,26 @@
 from marshmallow import Schema, fields
 
-#dump_only is used for data
-class ItemSchema(Schema):
+#dump_only is used for data that should not be modified by the user
+#load_only is used for data that should only be provided by the user
+class PlainItemSchema(Schema):
     id = fields.Str(dump_only=True)
     name = fields.Str(required=True)
     price = fields.Float(required=True)
-    store_id = fields.Str(required=True)
+
+class PlainStoreSchema(Schema):
+    id = fields.Str(dump_only=True)
+    name = fields.Str(required=True)
+
 
 class ItemUpdateSchema(Schema):
     name = fields.Str()
     price = fields.Float()
+    store_id = fields.Int() 
 
-class StoreSchema(Schema):
-    id = fields.Str(dump_only=True)
-    name = fields.Str(required=True)
+#Nested is used to include related objects in the schema
+class ItemSchema(PlainItemSchema):
+    store_id = fields.Int(required=True, load_only=True)
+    store = fields.Nested(PlainStoreSchema(), dump_only=True)
+
+class StoreSchema(PlainStoreSchema):
+    items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
